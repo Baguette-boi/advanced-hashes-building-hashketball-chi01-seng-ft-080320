a0e8757cc6a703854be497e405db490e986f0ea9
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-30 01:08:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       :players => [ 
         {
           :player_name => "Alan Anderson",
-          number: 0,
+          :number => 0,
           :shoe => 16,
           :points => 22,
           :rebounds => 12,
@@ -17,7 +17,7 @@ def game_hash
         },
         {
           :player_name => "Reggie Evans"   
-          number: 30,
+          :number => 30,
           :shoe => 14,
           :points => 12,
           :rebounds => 12,
