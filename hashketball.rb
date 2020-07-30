750e8413425fd43055e9e73edd2962358c43c58b
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-30 00:40:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,7 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players = [
+      :players => [
         {
         :player_name =>"Jeff Adrien",
           :number => 4,
